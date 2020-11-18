fcf04d9ed960d13e32b9e78763d52c27969f07d4
pablo <pblolara21@gmail.com>
2020-11-18 13:12:29 -0300

    Cambio a entorno wsl (ubuntu). Funcionando!

--- src/migrations/Migration20201118160424.ts
+++ src/migrations/Migration20201118160424.ts
@@ -1,6 +1,6 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20201118050829 extends Migration {
+export class Migration20201118160424 extends Migration {
 
   async up(): Promise<void> {
 
